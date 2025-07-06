914a216d47dfea677d96091bffa0623e3a65138b
aneesa <aneesa8685@bazeuniversity.edu.ng>
2025-07-06 23:35:25 +0100

    format datetime objrcts

--- NumbersDates/FormatDates/Program.cs
+++ NumbersDates/FormatDates/Program.cs
@@ -1,28 +1,33 @@
 ﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
 // Example file for formatting date information
 
+using System.Globalization;
+
 // Define a date
 DateTime AprFools = new DateTime(2025, 4, 1, 13, 23, 30);
 
-// TODO: 'd' Short date: mm/dd/yyyy (or dd/mm depending on locale)
+// 'd' Short date: mm/dd/yyyy (or dd/mm depending on locale)
+Console.WriteLine($"{AprFools:d}");
 
+// 'D' Full date: mm/dd/yyyy (or dd/mm depending on locale)
+Console.WriteLine($"{AprFools:D}");
 
-// TODO: 'D' Full date: mm/dd/yyyy (or dd/mm depending on locale)
+// 'f' Full date, short time
+Console.WriteLine($"{AprFools:f}");
 
+// 'F' Full date, long time
+Console.WriteLine($"{AprFools:F}");
 
-// TODO: 'f' Full date, short time
+// 'g' General date and time
+Console.WriteLine($"{AprFools:g}");
 
+// 'G' General date and time
+Console.WriteLine($"{AprFools:G}");
 
-// TODO: 'F' Full date, long time
+// Format using a specific CultureInfo (German culture for example)
+Console.WriteLine(AprFools.ToString("d", CultureInfo.CreateSpecificCulture("de-DE")));
 
-
-// TODO: 'g' General date and time
-
-
-// TODO: 'G' General date and time
-
-
-// TODO: Format using a specific CultureInfo
-
-
-// TODO: Defining custom date and time formats
+// Defining custom date and time formats
+Console.WriteLine($"{AprFools:dddd, MMMM d yyyy}");
+Console.WriteLine($"{AprFools:ddd, h:mm:ss tt}");
+Console.WriteLine($"{AprFools:MMM d yyyy}");
